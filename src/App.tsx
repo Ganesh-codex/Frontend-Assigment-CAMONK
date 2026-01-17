b4c3163fc65690d8617c735e0434d008485bf558
Ganesh Pandey <gp1852921@gmail.com>
2026-01-17 11:04:48 +0530

    include the home component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <BlogHeader />
+      {/* <BlogHeader /> */}
 
       <main className="flex-1">
         <Home />
